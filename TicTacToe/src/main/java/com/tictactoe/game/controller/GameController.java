ea8683829c69e01559f3caaad15c68fa3fee7fd2
Developer27 <developer2798@gmail.com>
2023-01-22 13:53:17 +0530

    feat : Added HttpStatus exception in response header

--- TicTacToe/src/main/java/com/tictactoe/game/controller/GameController.java
+++ TicTacToe/src/main/java/com/tictactoe/game/controller/GameController.java
@@ -4,11 +4,14 @@ package com.tictactoe.game.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tictactoe.game.response.ErrorResponse;
 import com.tictactoe.game.service.GameService;
+import com.tictcatoe.game.exception.InvalidTurnException;
 
 @RestController
 public class GameController {
@@ -25,4 +28,8 @@ public class GameController {
 		return ResponseEntity.status(HttpStatus.OK).body(gameService.playGame(player));
 	}
 
+	@ExceptionHandler(value = InvalidTurnException.class)
+	public ResponseEntity<ErrorResponse> handleInvalidTurnException(InvalidTurnException ex) {
+		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ErrorResponse(ex.getMessage()));
+	}
 }
--- TicTacToe/src/test/java/com/tictactoe/game/controller/GameControllerTests.java
+++ TicTacToe/src/test/java/com/tictactoe/game/controller/GameControllerTests.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.tictactoe.game.service.GameService;
+import com.tictcatoe.game.exception.InvalidTurnException;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -29,6 +30,13 @@ class GameControllerTests {
 		when(gameService.playGame('X')).thenReturn("Player X moved first");
 
 		mockMvc.perform(post("/tictactoe/play/{player}", 'X')).andExpect(status().isOk());
+	}
+
+	@Test
+	public void playGameAPIShouldShowForbiddenHttpStatusWhenInValidMove() throws Exception {
+		when(gameService.playGame('O')).thenThrow(new InvalidTurnException("Player X should move first"));
+
+		mockMvc.perform(post("/tictactoe/play/{player}", 'O')).andExpect(status().isForbidden());
 	}
 
 }
