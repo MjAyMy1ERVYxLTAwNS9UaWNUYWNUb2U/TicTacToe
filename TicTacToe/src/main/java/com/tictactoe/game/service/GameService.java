d63debce753d8ca626027fafedc023766abd1449
Developer27 <developer2798@gmail.com>
2023-01-22 23:01:05 +0530

    feat: Declare winner if any one of two diagonals is occupied by same player

--- TicTacToe/src/main/java/com/tictactoe/game/service/GameService.java
+++ TicTacToe/src/main/java/com/tictactoe/game/service/GameService.java
@@ -24,8 +24,9 @@ public class GameService {
 	public GameResponse playGame(Player player, int position) {
 		validateCurrentTurn(player, position);
 		savePlayerOnBoard(player, position);
-		if (gameBoard.isAnyOneOfThreeRowsOccupiedBySamePlayer()
-				|| gameBoard.isAnyOfThreeColumnsOccupiedBySamePlayer()) {
+		if (gameBoard.isAnyOneOfThreeRowsOccupiedBySamePlayer() || gameBoard.isAnyOfThreeColumnsOccupiedBySamePlayer()
+				|| gameBoard.isLeftToRightDiagonalOccupiedBySamePlayer()
+				|| gameBoard.isRightToLeftDiagonalOccupiedBySamePlayer()) {
 			return new GameResponse(String.format("Player %s won the game", player), "GAME_OVER");
 		}
 		return new GameResponse("GAME_IN_PROGRESS", getNextPlayer(player), player);
