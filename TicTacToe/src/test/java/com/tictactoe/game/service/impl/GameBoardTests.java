995e16a6015e84b781b14780171b778a6ea2edaf
Developer27 <developer2798@gmail.com>
2023-01-22 17:01:08 +0530

    Introduced Before Annotation method to initialize service board class object.

--- TicTacToe/src/test/java/com/tictactoe/game/service/impl/GameBoardTests.java
+++ TicTacToe/src/test/java/com/tictactoe/game/service/impl/GameBoardTests.java
@@ -2,6 +2,7 @@ package com.tictactoe.game.service.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -11,16 +12,21 @@ import com.tictactoe.game.enumeration.Player;
 @RunWith(MockitoJUnitRunner.class)
 public class GameBoardTests {
 
+	private GameBoard gameBoard;
+
+	@Before
+	public void setUp() {
+		gameBoard = new GameBoard();
+	}
+
 	@Test
 	public void playerXShouldBeMarkedInGameBoardWithGivenPosition() {
-		GameBoard gameBoard = new GameBoard();
 		gameBoard.setPlayerInPosition(0, 1, Player.X);
 		assertThat(gameBoard.getPlayerInPosition(0, 1)).isEqualTo(Player.X.getValue());
 	}
 
 	@Test
 	public void getCountPositionsOccupiedOnGameBoard() {
-		GameBoard gameBoard = new GameBoard();
 		assertThat(gameBoard.getCountOfPositionsOccupied()).isZero();
 	}
 }
