1bc240d62bfb19aa014786b134d275943b145da5
Developer27 <developer2798@gmail.com>
2023-01-23 00:40:40 +0530

    Refactor: Extracting common prefix for the routes of Game Controller

--- TicTacToe/src/main/java/com/tictactoe/game/controller/GameController.java
+++ TicTacToe/src/main/java/com/tictactoe/game/controller/GameController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tictactoe.game.enumeration.Player;
@@ -14,6 +15,7 @@ import com.tictactoe.game.response.GameResponse;
 import com.tictactoe.game.service.GameService;
 
 @RestController
+@RequestMapping("/tictactoe")
 public class GameController {
 
 	@Autowired
@@ -23,13 +25,13 @@ public class GameController {
 		this.gameService = gameService;
 	}
 
-	@PostMapping(value = "/tictactoe/play/{player}/{position}")
+	@PostMapping(value = "/play/{player}/{position}")
 	public ResponseEntity<GameResponse> playGameHandler(@PathVariable(name = "player") Player player,
 			@PathVariable(name = "position") int position) {
 		return ResponseEntity.status(HttpStatus.OK).body(gameService.playGame(player, position));
 	}
 
-	@GetMapping(value = "/tictactoe/resetgame")
+	@GetMapping(value = "/resetgame")
 	public ResponseEntity<String> resetGameHandler() {
 		return ResponseEntity.status(HttpStatus.OK).body(gameService.resetGame());
 	}
