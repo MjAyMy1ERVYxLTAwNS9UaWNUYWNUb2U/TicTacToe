c701eda90301003f98e6e98cf298bdbe74c7cfff
Developer27 <developer2798@gmail.com>
2023-01-22 19:17:48 +0530

    Refactor: Adding controller advice for handling all exceptions in application globally

--- TicTacToe/src/main/java/com/tictactoe/game/controller/GameController.java
+++ TicTacToe/src/main/java/com/tictactoe/game/controller/GameController.java
@@ -4,18 +4,13 @@ package com.tictactoe.game.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tictactoe.game.enumeration.Player;
-import com.tictactoe.game.response.ErrorResponse;
 import com.tictactoe.game.response.GameResponse;
 import com.tictactoe.game.service.GameService;
-import com.tictcatoe.game.exception.InvalidPositionException;
-import com.tictcatoe.game.exception.InvalidTurnException;
-import com.tictcatoe.game.exception.PositionOccupiedException;
 
 @RestController
 public class GameController {
@@ -33,18 +28,4 @@ public class GameController {
 		return ResponseEntity.status(HttpStatus.OK).body(gameService.playGame(player, position));
 	}
 
-	@ExceptionHandler(value = InvalidTurnException.class)
-	public ResponseEntity<ErrorResponse> handleInvalidTurnException(InvalidTurnException ex) {
-		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ErrorResponse(ex.getMessage()));
-	}
-
-	@ExceptionHandler(value = PositionOccupiedException.class)
-	public ResponseEntity<ErrorResponse> handlePositionOccupiedException(PositionOccupiedException ex) {
-		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ErrorResponse(ex.getMessage()));
-	}
-
-	@ExceptionHandler(value = InvalidPositionException.class)
-	public ResponseEntity<ErrorResponse> handleInvalidPositionException(InvalidPositionException ex) {
-		return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ErrorResponse(ex.getMessage()));
-	}
 }
