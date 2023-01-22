a1d36ebe854bc3307ddda28aaf9f9837ec33008d
Developer27 <developer2798@gmail.com>
2023-01-22 20:21:33 +0530

    Refactor : Extracting method to get if any row is occupied by the same player

--- TicTacToe/src/main/java/com/tictactoe/game/service/GameService.java
+++ TicTacToe/src/main/java/com/tictactoe/game/service/GameService.java
@@ -24,8 +24,7 @@ public class GameService {
 	public GameResponse playGame(Player player, int position) {
 		validateCurrentTurn(player, position);
 		savePlayerOnBoard(player, position);
-		if (gameBoard.isFirstRowOccupiedBySamePlayer() || gameBoard.isSecondRowOccupiedBySamePlayer()
-				|| gameBoard.isThirdRowOccupiedBySamePlayer()) {
+		if (gameBoard.isAnyOneOfThreeRowsOccupiedBySamePlayer()) {
 			return new GameResponse(String.format("Player %s won the game", player), "GAME_OVER");
 		}
 		return new GameResponse("GAME_IN_PROGRESS", getNextPlayer(player), player);
